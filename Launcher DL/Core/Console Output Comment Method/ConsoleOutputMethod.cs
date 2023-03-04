b86fceebd5b9181fdbc5b4ee66465a4a8d02c025
Ichimaki <ichimakiKasura@gmail.com>
2023-03-05 03:39:38 +0800

    Few changes on ConsoleOutputMethod

--- Launcher DL/Core/Console Output Comment Method/ConsoleOutputMethod.cs
+++ Launcher DL/Core/Console Output Comment Method/ConsoleOutputMethod.cs
@@ -1,16 +1,23 @@
-﻿namespace Launcher_DL.Core.Console_Output_Comment_Method;
+﻿using static Launcher_DL.Core.Console_Output_Comment_Method.CONSOLEOUTPUTMETHOD_STR;
+namespace Launcher_DL.Core.Console_Output_Comment_Method;
 
-internal class ConsoleOutputMethod
+internal static class ConsoleOutputMethod
 {
+	/// <summary>
+    /// extend ConsoleControl
+    /// </summary>
+    public static void DLAddConsole(this ConsoleControl console,string Type, string FormattedText, bool Italic = false, bool NoNL = false) => console.AddFormattedText($"{Type} {FormattedText}", Italic, NoNL);
+
 	public static void StartUpOutputComments()
 	{
-		console.AddFormattedText("<Gray%15>堂主の私に何か用かな？あれ、知らなかった？私が往生堂七十七代目堂主、胡桃だよ！でもあなた、ツヤのある髪に健康そうな体してる･･･そっか！仕事以外で私に用があるってことだね？<>");
+		console.AddFormattedText(CONSOLE_START);
 		console.Break("Gray");
 
 		console.AddFormattedText("<>welcome, <#ff4747%20|ExtraBlack>Hutao <>here!");
 
+		// we do a little troll
 		#if !DEBUG
-			//InitiateTheWindow.InitiateMePlease = "Initiate";
+			InitiateTheWindow.InitiateMePlease = "Initiate";
 		#endif
 	}
 
@@ -22,19 +29,23 @@ internal class ConsoleOutputMethod
 		{
 			switch(IsSuccess)
 			{
-				case 0:
-					console.AddFormattedText($"<#a85192%14>[SYSTEM] <Green%14>SUCCESS <Gray%14>Config loaded");
+				case 0: console.DLAddConsole(CONSOLE_SYSTEM_STRING, "<Green%14>SUCCESS <Gray%14>Config loaded");
 				break;
 
 				case 1:
-					console.AddFormattedText($"<#a85192%14>[SYSTEM] <Red%14>FAILED <Gray%14>Error on loading [ {Name} ]");
+					console.DLAddConsole(CONSOLE_SYSTEM_STRING, $"<Red%14>FAILED <Gray%14>Error on loading [ {Name} ]");
 					console.AddFormattedText($"<DimGray%12>ERROR: {error}", true);
 				break;
 
-				case 2:
-					console.AddFormattedText($"<#a85192%14>[SYSTEM] <Red%14>FAILED <Gray%14>Default Config loaded");
+				case 2: console.DLAddConsole(CONSOLE_SYSTEM_STRING,"<Red%14>FAILED <Gray%14>Default Config loaded");
 				break;
 			}
 		}
+	}
+
+	public static void TypeChangedOutputComment()
+	{
+		if (config.ShowSystemOutput)
+			console.DLAddConsole(CONSOLE_SYSTEM_STRING,$"<Gray%14>Changed TYPE to {comboBoxType.GetItemContent}");
 	}
 }
---
+++ Launcher DL/Core/Console Output Comment Method/ConsoleOutputMethodSTR.cs
@@ -0,0 +1,10 @@
+namespace Launcher_DL.Core.Console_Output_Comment_Method;
+
+public abstract class CONSOLEOUTPUTMETHOD_STR
+{
+    public const string CONSOLE_START = "<Gray%15>堂主の私に何か用かな？あれ、知らなかった？私が往生堂七十七代目堂主、胡桃だよ！でもあなた、ツヤのある髪に健康そうな体してる･･･そっか！仕事以外で私に用があるってことだね？<>"; 
+    public const string CONSOLE_SYSTEM_STRING = "<#a85192%14>[SYSTEM]";
+    public const string CONSOLE_ERROR_STRING = "<Red>[ERROR]";
+    public const string CONSOLE_WARNING_STRING = "<Yellow>[WARNING]";
+    public const string CONSOLE_ERROR_SOFT_STRING = "<Orange>[ERROR]";
+}
