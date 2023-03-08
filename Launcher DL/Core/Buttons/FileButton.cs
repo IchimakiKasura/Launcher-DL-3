ef8f239feedc2646a61cf5dbb5a2ea8649916eb6
Ichimaki <ichimakiKasura@gmail.com>
2023-03-09 04:32:21 +0800

    Removed SelfLauncher

--- Launcher DL/Core/Buttons/FileButton.cs
+++
@@ -1,9 +0,0 @@
-namespace Launcher_DL.Core.Buttons;
-
-public class FileButton
-{
-    public static void ButtonClicked(object s, RoutedEventArgs e)
-    {
-        WindowsComponents.FreezeComponents();
-    }
-}
--- Launcher DL/Core/Buttons/UpdateButton.cs
+++
@@ -1,9 +0,0 @@
-namespace Launcher_DL.Core.Buttons;
-
-public class UpdateButton
-{
-    public static void ButtonClicked(object s, RoutedEventArgs e)
-    {
-        WindowsComponents.FreezeComponents();
-    }
-}
