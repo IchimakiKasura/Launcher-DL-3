7d0be53869edb15701e30b47433bffc2d7b7675c
Ichimaki <ichimakiKasura@gmail.com>
2023-03-17 07:08:55 +0800

    Sorry, Im not sorry
    Sorry to make your eyes blind.
    For I have done

--- src/LauncherDL/Core/Console/ErrorOutput.cs
+++ src/LauncherDL/Core/Console/ErrorOutput.cs
@@ -1,11 +1,14 @@
 namespace LauncherDL.Core.ConsoleDL                                                                                             ;
 internal partial class ConsoleLive                                                                                              {
     public static int SelectedError = 0                                                                                         ;
-    public static void ErrorOutputComment(object s, DataReceivedEventArgs e)                                                    {
+    public static void ErrorOutputComment(object s, DataReceivedEventArgs e)                                                    =>
+        DL_Dispatch.Invoke(()=>Error_Invoked(e.Data),e.Data)                                                                    ;
+
+    static void Error_Invoked(string StringData)                                                                                {
         switch(SelectedError)                                                                                                   {
             case 0                                                                                                              :
                 console.DLAddConsole(CONSOLE_ERROR_STRING,
-                $"<%14>1. Link is unavailable$nl$2. Link is invalid$nl$3. Check internet connection.")                          ;
+                $"<%14>1. Link is unavailable$nl$2. Link is invalid$nl$3. Check internet connection.");
             break                                                                                                               ;
             case 1                                                                                                              :
                 console.DLAddConsole(CONSOLE_ERROR_STRING,
